491096a30bac4f26420cd1a00a160945d8342eda
NULL <68575510+SOMENULL@users.noreply.github.com>
2023-08-30 01:11:50 -0500

    All Main Files

---
+++ Gliding [Tutorial]/Assets/Scripts/CameraTracking.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CameraTracking : MonoBehaviour
+{
+    [SerializeField] private Transform PlayerTransform;
+
+    [SerializeField] private float SmoothTime;
+    [SerializeField] private float Sensitivity;
+    [SerializeField] private float MaxViewRange;
+    private float mouseX, mouseY;
+
+    [SerializeField] private Vector3 Offset;
+    private Vector3 CurrentVelocity;
+
+    private void FixedUpdate()
+    {
+        FollowTargetTransform();
+    }
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void FollowTargetTransform()
+    {
+        Vector3 desiredPosition = PlayerTransform.position + Offset;
+        Vector3 positionInterpolation = Vector3.SmoothDamp(transform.position, desiredPosition, ref CurrentVelocity, SmoothTime);
+
+        transform.position = positionInterpolation;
+    }
+    private void CameraRotation()
+    {
+        mouseX += Input.GetAxisRaw("Mouse Y") * Time.deltaTime * Sensitivity;
+        mouseY += Input.GetAxisRaw("Mouse X") * Time.deltaTime * Sensitivity;
+        float clampedX = Mathf.Clamp(mouseX, -MaxViewRange, MaxViewRange);
+
+        Quaternion targetRotation = Quaternion.Euler(clampedX, mouseY, transform.eulerAngles.z);
+        transform.rotation = targetRotation;
+    }
+}
---
+++ Gliding [Tutorial]/Assets/Scripts/GlidingSystem.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlidingSystem : MonoBehaviour
+{
+    [SerializeField] private float BaseSpeed = 30f;
+    [SerializeField] private float MaxThrustSpeed;
+    [SerializeField] private float MinThrustSpeed;
+    [SerializeField] private float ThrustFactor;
+    [SerializeField] private float DragFactor;
+    [SerializeField] private float MinDrag;
+    [SerializeField] private float RotationSpeed;
+    [SerializeField] private float TiltStrength = 90;
+    [SerializeField] private float LowPercent = 0.1f, HighPercent = 1;
+    
+    private float CurrentThrustSpeed;
+    private float TiltValue, LerpValue;
+
+    private Transform CameraTransform;
+    private Rigidbody Rb;
+
+    private void Start()
+    {
+        CameraTransform = Camera.main.transform.parent;
+        Rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        GlidingMovement();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) 
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        ManageRotation();
+    }
+
+    private void GlidingMovement() 
+    {
+        float pitchInDeg = transform.eulerAngles.x % 360;
+        float pitchInRads = transform.eulerAngles.x * Mathf.Deg2Rad;
+        float mappedPitch = -Mathf.Sin(pitchInRads);
+        float offsetMappedPitch = Mathf.Cos(pitchInRads) * DragFactor;
+        float accelerationPercent = pitchInDeg >= 300f ? LowPercent : HighPercent; 
+        Vector3 glidingForce = -Vector3.forward * CurrentThrustSpeed;
+
+        CurrentThrustSpeed += mappedPitch * accelerationPercent * ThrustFactor * Time.deltaTime;
+        CurrentThrustSpeed = Mathf.Clamp(CurrentThrustSpeed, 0, MaxThrustSpeed);
+
+        if (Rb.velocity.magnitude >= MinThrustSpeed)
+        {
+            Rb.AddRelativeForce(glidingForce);
+            Rb.drag = Mathf.Clamp(offsetMappedPitch, MinDrag, DragFactor);
+        }
+        else 
+        {
+            CurrentThrustSpeed = 0;
+        }
+
+        Debug.Log(CurrentThrustSpeed);
+    }
+    private void ManageRotation() 
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        TiltValue += mouseX * TiltStrength * Time.deltaTime;
+
+        if (mouseX == 0)
+        {
+            TiltValue = Mathf.Lerp(TiltValue, 0, LerpValue);
+            LerpValue += Time.deltaTime;
+        }
+        else 
+        {
+            LerpValue = 0;
+        }
+
+        Quaternion targetRotation = Quaternion.Euler(CameraTransform.eulerAngles.x, CameraTransform.eulerAngles.y, TiltValue);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
+    }
+}
